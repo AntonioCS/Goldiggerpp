c498dfbce7b7e7f42c389a90d7a64a174178b053
AntonioCS <antoniocs@gmail.com>
2018-06-17 22:58:07 +0100

    Moving game logic here

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,11 +1,58 @@
 #include "Game.h"
+#include <sstream> //istringstream
+#include <fstream> //ifstream
+#include <stdexcept>
 
+#include "AcsGE/ECS/Entity.h"
+#include "AcsGE/ECS/Entity.h"
+#include "AcsGE/ECS/Components/RenderableComponent.h"
+#include "AcsGE/ECS/Components/SpriteComponent.h"
+#include "CustomComponents/MapItemComponent.h"
 
-
-Game::Game()
+void Game::generateMap(std::string pathToMap)
 {
-}
+	std::ifstream mapFile{ pathToMap };
 
+	if (mapFile.is_open()) {
+		std::string line;
+		int moveBy = 64;
+		int currentMoveByY = 0;
+
+		while (std::getline(mapFile, line))
+		{
+			int currentMoveByX = 0;
+			int mapValue;
+			std::istringstream mapLineStream(line);
+
+			while (mapLineStream >> mapValue) {
+				if (mapValue != 0) {
+					auto &e = m_em.make_entity();
+
+					e.addComponent<AcsGameEngine::ECS::SpriteComponent>(
+						m_spritesTexture,
+						m_spriteMappings[m_mapMappings[mapValue]],
+						currentMoveByX,
+						currentMoveByY
+					);
+					e.addComponent<AcsGameEngine::ECS::RenderableComponent>();
+					e.addComponent<MapItemComponent>();
+				}
+				currentMoveByX += moveBy;
+			}
+
+			currentMoveByX = 0;
+			currentMoveByY += moveBy;
+		}
+	}
+	else {
+		throw std::runtime_error(std::string{ "Unable to open map file: " + pathToMap });
+	}
+}
+Game::Game(AcsGameEngine::Renderer &renderer, AcsGameEngine::ECS::EntityManager &em) : m_renderer(renderer), m_em(em)
+{
+	m_spritesTexture = m_renderer.make_texture(m_imagesPaths["spritesSheet"]);
+	//auto backgroundTexture = renderer.make_texture("assets/images/cave-background.jpg");
+}
 
 Game::~Game()
 {
--- src/Game.h
+++ src/Game.h
@@ -6,22 +6,60 @@
 #include "AcsGE/Util/ColorList.h"
 #include "AcsGE/Window.h"
 #include "AcsGE/Texture.h"
-//#include "src/Game/EntitySprite.h"
-#include "AcsGE/ECS/Entity.h"
-#include "AcsGE/ECS/Components/SpriteComponent.h"
-
+#include "AcsGE/ECS/EntityManager.h"
 
 namespace {
 	constexpr int mapLength = 20;
 	constexpr int mapHeight = 10;
-
-	using EntityMap = std::array<std::array<std::unique_ptr<AcsGameEngine::ECS::Entity>, mapLength>, mapHeight>;
 }
 
 class Game
 {
+	std::unordered_map<std::string, std::string> m_imagesPaths{
+		{"spritesSheet", "assets/images/Sprites.png" },
+		{"background_cave", "assets/images/cave-background.jpg"},
+		{"background_cave2", "assets/images/cave-background2.jpg"},
+		{"background_snowy", "assets/images/snowy-background.jpg"}
+	};
+
+	std::unordered_map<std::string, std::string> m_levelsPaths{
+		{ "level1", "assets/levels/level1" },
+		{ "level2", "assets/levels/level2" },
+		{ "level3", "assets/levels/level3" },
+		{ "level4", "assets/levels/level4" }
+	};
+
+	std::unordered_map<int, std::string> m_mapMappings{
+		{ 1, "block_grass" },
+		{ 2, "block" },
+		{ 4, "gold" },
+		{ 3, "green_ladder" },
+		{ 9, "enemy_snake" },
+		{ 5, "portal_blue" }
+	};
+	std::unordered_map<std::string, SDL_Rect> m_spriteMappings{
+		{ "block_grass",{ 0, 383, 64, 64 } },
+		{ "block",{ 64, 384, 64, 64 } },
+		{ "block_snow",{ 128, 383, 64, 64 } },
+		{ "gold",{ 192,383,64,64 } },
+		{ "green_ladder",{ 256, 384, 64, 64 } },
+		{ "spikes",{ 320, 383, 64, 64 } },
+		{ "ladder",{ 384, 383, 64, 64 } },
+		{ "player",{ 0, 0, 45, 64 } },
+		{ "enemy_snake",{ 0, 455, 55, 56 } },
+		{ "porta_blue",{ 0, 710, 59, 120 } }
+	};
+
+	size_t mapBlocks = 64;
+	AcsGameEngine::Renderer &m_renderer;
+	AcsGameEngine::ECS::EntityManager &m_em;
+	AcsGameEngine::Texture m_spritesTexture{ nullptr };
+	const int m_mapLength = 20;
+	const int m_mapHeight = 10;
+
+	void generateMap(std::string pathToMap);
 public:
-	Game();
+	Game(AcsGameEngine::Renderer &renderer, AcsGameEngine::ECS::EntityManager &em);
 	~Game();
 };
 
