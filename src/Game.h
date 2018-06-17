62176cb31513f7d16a943362fdb80a99abcda36c
AntonioCS <antoniocs@gmail.com>
2018-06-17 10:43:46 +0100

    Moving a things to here

--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,23 @@
 #pragma once
 
 
+#include "AcsGE/EventManager.h"
+#include "AcsGE/Renderer.h"
+#include "AcsGE/Util/ColorList.h"
+#include "AcsGE/Window.h"
+#include "AcsGE/Texture.h"
+//#include "src/Game/EntitySprite.h"
+#include "AcsGE/ECS/Entity.h"
+#include "AcsGE/ECS/Components/SpriteComponent.h"
+
+
+namespace {
+	constexpr int mapLength = 20;
+	constexpr int mapHeight = 10;
+
+	using EntityMap = std::array<std::array<std::unique_ptr<AcsGameEngine::ECS::Entity>, mapLength>, mapHeight>;
+}
+
 class Game
 {
 public:
