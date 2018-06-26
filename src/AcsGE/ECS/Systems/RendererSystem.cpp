d53b85d852453a5de09dca8f030e76399aaf01a5
AntonioCS <antoniocs@gmail.com>
2018-06-26 07:35:13 +0100

    Base system

---
+++ src/AcsGE/ECS/Systems/RendererSystem.cpp
@@ -0,0 +1,32 @@
+#include "RendererSystem.h"
+#include "../Components/RenderableComponent.h"
+
+namespace AcsGameEngine::ECS {
+
+	RendererSystem::RendererSystem(EntityManager & em, Renderer & renderer) : System(em), m_renderer(renderer) {}
+
+	RendererSystem::~RendererSystem()
+	{
+	}
+
+	void RendererSystem::init()
+	{
+		m_vecRef = m_entityManager.findByComponent<RenderableComponent, SpriteComponent>();
+	}
+
+	void RendererSystem::update(float dt)
+	{
+	}
+
+	void RendererSystem::render()
+	{
+		for (const auto ref : m_vecRef) {
+			auto &e = ref.get();
+			auto state = e.getComponent<RenderableComponent>();
+
+			if (state.isVisible()) {
+				m_renderer.DrawSprite(e.getComponent<SpriteComponent>().getSprite());
+			}
+		}
+	}
+}
---
+++ src/AcsGE/ECS/Systems/RendererSystem.h
@@ -0,0 +1,25 @@
+#pragma once
+
+
+#include "../System.h"
+#include "../EntityManager.h"
+#include "../Entity.h"
+#include "../Components/SpriteComponent.h"
+#include "../../Renderer.h"
+
+namespace AcsGameEngine::ECS {
+
+	class RendererSystem : public System
+	{
+		Renderer &m_renderer;
+	public:
+		RendererSystem(EntityManager &em, Renderer &renderer);
+		~RendererSystem();
+
+		void init();
+
+		void update(float dt);
+
+		void render();
+	};
+}
