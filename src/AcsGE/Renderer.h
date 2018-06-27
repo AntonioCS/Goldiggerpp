34a2e8ecc4af6479faf55cc225a13ff1718c6df6
AntonioCS <antoniocs@gmail.com>
2018-06-27 06:59:05 +0100

    Removed texture factory and moved some stuff to cpp

--- src/AcsGE/Renderer.h
+++ src/AcsGE/Renderer.h
@@ -16,64 +16,13 @@ class Texture;
 class Sprite;
 
 class Renderer {
-
-private:
-    class TextureFactory {
-    private:
-        const Renderer &m_renderer;
-        std::unordered_map<std::string, std::weak_ptr<Texture>> m_items;
-
-    public:
-        TextureFactory(const Renderer &r) : m_renderer(r) {
-
-        }		
-
-        std::shared_ptr<Texture> getTexture(const std::string& path)
-        {
-            auto res = m_items.find(path);
-            std::shared_ptr<Texture> item;
-            auto func = [this, &item](const std::string& path) {
-                SDL_Surface* tmp = IMG_Load(path.c_str());
-                SDL_Texture* t = SDL_CreateTextureFromSurface(m_renderer.getRawPointer(), tmp);
-                SDL_FreeSurface(tmp);
-
-                item = std::make_shared<Texture>(t);
-                m_items[path] = std::weak_ptr<Texture>(item);
-            };
-
-            if (res == m_items.end()) {
-                func(path);
-            }
-            else {
-                item = res->second.lock();
-                if (item == nullptr) {
-                    func(path);
-                }
-            }
-
-            return item;
-        }
-    };    
-
 public:
     explicit Renderer(const Window& window, int index = -1, Uint32 flags = SDL_RENDERER_ACCELERATED);
     Renderer(const Renderer& orig) = delete;
     Renderer(Renderer &&) = default;
-    Renderer& operator=(const Renderer& other)
-    {
-        if (&other != this) {
-            
-        }
-        return *this;
-    }
-    Renderer& operator=(Renderer && other)
-    {
-        if (&other != this) {
-            
-        }
-        return *this;
-    }
-    
+	Renderer& operator=(const Renderer& other);
+	Renderer& operator=(Renderer && other);
+
     void DrawColor(const Util::ColorList::Color& /*color*/, uint8_t alpha = 255) const;
     void DrawColor(uint8_t r, uint8_t g, uint8_t b, uint8_t alpha = 255) const;
     void Clear() const;
@@ -81,7 +30,7 @@ public:
     void Present() const;
     void DrawRect(SDL_Rect, const Util::ColorList::Color &c = Util::ColorList::_nocolor, bool fill = false) const;
     void DrawSprite(const Sprite &) const noexcept;
-    
+
 	Texture make_texture(const std::string & path);
 
     SDL_Renderer* getRawPointer() const;
@@ -89,9 +38,8 @@ public:
 private:
     std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)> m_renderer;
     const Window& m_window;
-    TextureFactory m_tf;
     SDL_Renderer* createRendererPointer(SDL_Window* w, int index, Uint32 flags);
-    
+
 };
 } //namespace AcsGameEngine
 
