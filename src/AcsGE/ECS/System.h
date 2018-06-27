703eadb44225fdfe8acf83346cf0d0abe29b86a1
AntonioCS <antoniocs@gmail.com>
2018-06-27 06:56:32 +0100

    System base class

--- src/AcsGE/ECS/System.h
+++ src/AcsGE/ECS/System.h
@@ -1,7 +1,25 @@
 #pragma once
 
+#include "EntityManager.h"
+#include "Entity.h"
+
 namespace AcsGameEngine::ECS {
 	class System {
+	protected:
+		using VecEntityRef = std::vector<std::reference_wrapper<Entity>>;
 
+		EntityManager &m_entityManager;
+		VecEntityRef m_vecRef;
+
+	public:
+		System(EntityManager &em) : m_entityManager(em) {}
+
+		virtual ~System() {}
+
+		virtual void init() = 0;
+
+		virtual void update(float deltaTime) = 0;
+
+		virtual void render() = 0;
 	};
 }
