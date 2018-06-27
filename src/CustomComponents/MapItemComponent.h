7a19afb12f65563eecd012e7d9b21465aed49b83
AntonioCS <antoniocs@gmail.com>
2018-06-27 07:00:51 +0100

    Added constructor

--- src/CustomComponents/MapItemComponent.h
+++ src/CustomComponents/MapItemComponent.h
@@ -3,5 +3,13 @@
 #include "../AcsGE/ECS/Component.h"
 
 class MapItemComponent : public AcsGameEngine::ECS::Component {
+public:
+	MapItemComponent(int x, int y, int w, int h) : x(x), y(y), w(w), h(h) {}
 
+	int x;
+	int y;
+	int w;
+	int h;
+
+	bool fill{ false };
 };
