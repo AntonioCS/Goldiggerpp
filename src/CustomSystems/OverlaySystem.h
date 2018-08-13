0802d9b75c2702dba4410b9268d80a3559080fe3
AntonioCS <antoniocs@gmail.com>
2018-08-13 22:03:25 +0100

    Passing in more things

--- src/CustomSystems/OverlaySystem.h
+++ src/CustomSystems/OverlaySystem.h
@@ -2,15 +2,17 @@
 
 #include "../AcsGE/ECS/System.h"
 #include "../AcsGE/ECS/Entity.h"
+#include "../AcsGE/EventManager.h"
 
 class OverlaySystem : public AcsGameEngine::ECS::System
 {
 	AcsGameEngine::ECS::Entity *m_previousEntity = nullptr;
+	AcsGameEngine::EventManager &m_eventManager;
 
 public:
-	using System::System;
+	//using System::System;
 
-	//OverlaySystem(AcsGameEngine::ECS::EntityManager &em) : System(em) {}
+	OverlaySystem(AcsGameEngine::ECS::EntityManager &em, AcsGameEngine::EventManager &eventManager) : System(em), m_eventManager(eventManager) {}
 	
 	void init();
 
