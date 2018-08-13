4a5794732a95c5453e37fded0dc930f759b3a4fc
AntonioCS <antoniocs@gmail.com>
2018-08-13 21:58:42 +0100

    Added using

--- src/AcsGE/ECS/System.h
+++ src/AcsGE/ECS/System.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <chrono>
+
 #include "EntityManager.h"
 #include "Entity.h"
 
 namespace AcsGameEngine::ECS {
+	using VecEntityRef = std::vector<std::reference_wrapper<Entity>>;
+
 	class System {
 	protected:
-		using VecEntityRef = std::vector<std::reference_wrapper<Entity>>;
-
 		EntityManager &m_entityManager;
 		VecEntityRef m_vecRef;
 
@@ -18,6 +20,7 @@ namespace AcsGameEngine::ECS {
 
 		virtual void init() = 0;
 
+		//virtual void update(std::chrono::milliseconds deltaTime) = 0;
 		virtual void update(float deltaTime) = 0;
 
 		virtual void render() = 0;
